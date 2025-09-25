84cd6aa148578671f2b027ce158a61840b80a7fe
JullianeSSousa <jullianecoda@gmail.com>
2025-09-24 21:45:03 -0300

    iniciando swicth case

--- desafio-semanal/desafio.ts
+++ desafio-semanal/desafio.ts
@@ -1,9 +1,11 @@
 interface aluno {
     nome: string;
     cursoId: number;
+    nomeCurso: string;
     notas: number[];
     mensagem: string;
 }
+
 
 function mediaAluno(aluno: aluno): number {
 
@@ -33,6 +35,7 @@ let aluno1: aluno =
 {
     nome: "Julliane Sousa",
     cursoId: 123,
+    nomeCurso:"Programação",
     notas: [10, 9, 9],
     mensagem: "Parabéns, você foi aprovada!",
 };
@@ -40,6 +43,7 @@ let aluno2: aluno =
 {
     nome: "Janjo Santos",
     cursoId: 453,
+    nomeCurso:"Ed. Fisica",
     notas: [10, 5, 3],
     mensagem: "Aluno na final,continue se esforçando!",
 };
@@ -48,8 +52,29 @@ let aluno3: aluno = {
 
     nome: "Jair Anderson",
     cursoId: 454,
+    nomeCurso:"Engenharia",
     notas: [5, 7, 3],
     mensagem: "Infelizmente você foi reprovado!",
 };
 
 estiloAprendizO(aluno1)
+
+function imprimirCurso(aluno: aluno, aluno2: aluno, aluno3: aluno): void {
+  switch (aluno.cursoId) {
+    case 1:
+      console.log(aluno.nomeCurso + aluno.cursoId);
+      break;
+    case 2:
+      console.log(aluno.nomeCurso + aluno.cursoId);
+      break;
+    case 454: // seu caso específico
+      console.log(aluno.nomeCurso + aluno.cursoId);
+      break;
+    default:
+      console.log((aluno1), (aluno2), (aluno3));
+  }
+}
+
+imprimirCurso(aluno1, aluno2, aluno3);
+
+
